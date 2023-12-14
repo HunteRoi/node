e8459df6f97dd2a3c099b736eaae88a7aa3fb89f
hunteroi <etu33784@henallux.be>
2023-12-14 01:46:09 +0100

    feat(CreateIdea): store the idea into local db

--- src/application/use_cases/create_idea.py
+++ src/application/use_cases/create_idea.py
@@ -1,7 +1,9 @@
 from datetime import datetime
+from src.application.interfaces import iidea_repository
 
 from src.application.interfaces.icreate_idea import ICreateIdea
 from src.application.interfaces.iid_generator_service import IIdGeneratorService
+from src.application.interfaces.iidea_repository import IIdeaRepository
 from src.application.interfaces.imachine_service import IMachineService
 from src.domain.entities.idea import Idea
 
@@ -13,15 +15,20 @@ class CreateIdea(ICreateIdea):
         self,
         machine_service: IMachineService,
         id_generator_service: IIdGeneratorService,
+        idea_repository: IIdeaRepository,
     ):
         self.machine_service = machine_service
         self.id_generator_service = id_generator_service
+        self.idea_repository = idea_repository
 
     def execute(self, community_id: str, content: str) -> Idea:
         """Create an idea."""
         author = self.machine_service.get_current_user(community_id)
         creation_date = datetime.now()
 
-        return Idea(
+        idea = Idea(
             self.id_generator_service.generate(), content, author, creation_date
         )
+        self.idea_repository.add_idea_to_community(community_id, idea)
+
+        return idea
--- tests/unit/application/use_cases/test_create_idea.py
+++ tests/unit/application/use_cases/test_create_idea.py
@@ -1,4 +1,3 @@
-from platform import machine
 from unittest import mock
 from unittest.mock import MagicMock
 
@@ -9,6 +8,7 @@ from src.domain.entities.member import Member
 class TestCreateIdea:
     """Test suite around creating an idea."""
 
+    @mock.patch("src.application.interfaces.iidea_repository", name="idea_repo_mock")
     @mock.patch(
         "src.application.interfaces.iid_generator_service",
         name="id_generator_service_mock",
@@ -17,16 +17,24 @@ class TestCreateIdea:
         "src.application.interfaces.imachine_service", name="machine_service_mock"
     )
     def test_create_idea(
-        self, machine_service_mock: MagicMock, id_generator_service_mock: MagicMock
+        self,
+        machine_service_mock: MagicMock,
+        id_generator_service_mock: MagicMock,
+        idea_repo_mock: MagicMock,
     ):
         """Creating an idea should be possible given the proper arguments."""
         content = "content"
-        usecase = CreateIdea(machine_service_mock, id_generator_service_mock)
+        usecase = CreateIdea(
+            machine_service_mock,
+            id_generator_service_mock,
+            idea_repository=idea_repo_mock,
+        )
 
         idea = usecase.execute("1", content)
 
         assert idea is not None
 
+    @mock.patch("src.application.interfaces.iidea_repository", name="idea_repo_mock")
     @mock.patch(
         "src.application.interfaces.iid_generator_service",
         name="id_generator_service_mock",
@@ -35,16 +43,24 @@ class TestCreateIdea:
         "src.application.interfaces.imachine_service", name="machine_service_mock"
     )
     def test_create_idea_content_is_well_set(
-        self, machine_service_mock: MagicMock, id_generator_service_mock: MagicMock
+        self,
+        machine_service_mock: MagicMock,
+        id_generator_service_mock: MagicMock,
+        idea_repo_mock: MagicMock,
     ):
         """Creating an idea should be possible given the proper arguments."""
         content = "content"
-        usecase = CreateIdea(machine_service_mock, id_generator_service_mock)
+        usecase = CreateIdea(
+            machine_service_mock,
+            id_generator_service_mock,
+            idea_repository=idea_repo_mock,
+        )
 
         idea = usecase.execute("1", content)
 
         assert idea.content == content
 
+    @mock.patch("src.application.interfaces.iidea_repository", name="idea_repo_mock")
     @mock.patch(
         "src.application.interfaces.iid_generator_service",
         name="id_generator_service_mock",
@@ -53,14 +69,46 @@ class TestCreateIdea:
         "src.application.interfaces.imachine_service", name="machine_service_mock"
     )
     def test_create_idea_author_is_current_user(
-        self, machine_service_mock: MagicMock, id_generator_service_mock: MagicMock
+        self,
+        machine_service_mock: MagicMock,
+        id_generator_service_mock: MagicMock,
+        idea_repo_mock: MagicMock,
     ):
         """Creating an idea should be possible given the proper arguments."""
         content = "content"
         author = Member("auth_key", "127.0.0.1", 0)
         machine_service_mock.get_current_user.return_value = author
-        usecase = CreateIdea(machine_service_mock, id_generator_service_mock)
+        usecase = CreateIdea(
+            machine_service_mock,
+            id_generator_service_mock,
+            idea_repository=idea_repo_mock,
+        )
 
         idea = usecase.execute("1", content)
 
         assert idea.author == author
+
+    @mock.patch("src.application.interfaces.iidea_repository", name="idea_repo_mock")
+    @mock.patch(
+        "src.application.interfaces.iid_generator_service",
+        name="id_generator_service_mock",
+    )
+    @mock.patch(
+        "src.application.interfaces.imachine_service", name="machine_service_mock"
+    )
+    def test_create_idea_calls_repository(
+        self, machine_service_mock, id_generator_service_mock, idea_repo_mock
+    ):
+        """Creating an idea should be possible given the proper arguments."""
+        content = "content"
+        author = Member("auth_key", "127.0.0.1", 0)
+        machine_service_mock.get_current_user.return_value = author
+        usecase = CreateIdea(
+            machine_service_mock,
+            id_generator_service_mock,
+            idea_repository=idea_repo_mock,
+        )
+
+        idea = usecase.execute("1", content)
+
+        idea_repo_mock.add_idea_to_community.assert_called_once_with("1", idea)
